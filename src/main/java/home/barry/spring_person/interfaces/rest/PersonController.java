64957eb283265b884469ca526d03c67c597fbd17
Barry Yim <barryyim@Barrys-MacBook-Pro.local>
2019-05-19 16:24:18 +0800

    the first commit

---
+++ src/main/java/home/barry/spring_person/interfaces/rest/PersonController.java
@@ -0,0 +1,4 @@
+package home.barry.spring_person.interfaces.rest;
+
+public class PersonController {
+}
---
+++ src/main/java/home/barry/spring_person/model/Person.java
@@ -0,0 +1,32 @@
+package home.barry.spring_person.model;
+
+public class Person {
+
+    private String name;
+    private String info;
+
+    public Person(){
+
+    }
+
+    public Person(String name, String info){
+        this.name = name;
+        this.info = info;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
