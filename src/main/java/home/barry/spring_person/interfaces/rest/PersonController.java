dd1dbdf321cac814f3030fd27497ab6001c4b537
barryyim <bbarryyim@gmail.com>
2019-05-19 16:42:23 +0800

    With rest controller + Person Service

--- src/main/java/home/barry/spring_person/interfaces/rest/PersonController.java
+++ src/main/java/home/barry/spring_person/interfaces/rest/PersonController.java
@@ -1,4 +1,8 @@
 package home.barry.spring_person.interfaces.rest;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PersonController {
+
 }
---
+++ src/main/java/home/barry/spring_person/services/PersonService.java
@@ -0,0 +1,9 @@
+package home.barry.spring_person.services;
+
+import home.barry.spring_person.model.Person;
+
+public class PersonService {
+    public void addPerson(Person person){
+        person = new Person();
+    }
+}
