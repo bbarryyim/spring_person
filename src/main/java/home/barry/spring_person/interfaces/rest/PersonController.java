4bb5037b0338f0b00f1848d198afbf8ec81f3491
barryyim <bbarryyim@gmail.com>
2019-10-25 15:06:50 -0700

    with swagger 2 (2.7.0) and personService

--- src/main/java/home/barry/spring_person/interfaces/rest/PersonController.java
+++ src/main/java/home/barry/spring_person/interfaces/rest/PersonController.java
@@ -1,8 +1,27 @@
 package home.barry.spring_person.interfaces.rest;
 
+import home.barry.spring_person.model.Person;
+import home.barry.spring_person.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api/v1/person")
 public class PersonController {
 
+    @Autowired
+    PersonService personService;
+
+    @RequestMapping(value = "/add/", method = RequestMethod.POST)
+    public void createUser(@RequestBody Person person){
+        personService.addPerson(person);
+    }
+
+    @RequestMapping(value = "/remove/", method = RequestMethod.DELETE)
+    public void removeUser(@RequestBody Person person){
+        personService.removePerson(person);
+    }
 }
--- src/main/java/home/barry/spring_person/services/PersonService.java
+++ src/main/java/home/barry/spring_person/services/PersonService.java
@@ -1,9 +1,22 @@
 package home.barry.spring_person.services;
 
 import home.barry.spring_person.model.Person;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class PersonService {
+
+    List<Person> personList = new ArrayList<>();
+
     public void addPerson(Person person){
-        person = new Person();
+        personList.add(person);
     }
+
+    public void removePerson(Person person){
+        personList.remove(person);
+    }
+
 }
