60c81ba79d3eddd67c7165720981b0d4d14fcb78
barryyim <bbarryyim@gmail.com>
2019-10-28 13:30:49 -0700

    With changes on PeronService; finding person index and swagger ui

--- src/main/java/home/barry/spring_person/services/PersonService.java
+++ src/main/java/home/barry/spring_person/services/PersonService.java
@@ -9,14 +9,33 @@ import java.util.List;
 @Component
 public class PersonService {
 
+    public static final int PRODUCT_NOT_PRESENT = -1;
+
     List<Person> personList = new ArrayList<>();
 
     public void addPerson(Person person){
         personList.add(person);
     }
 
-    public void removePerson(Person person){
-        personList.remove(person);
+    public void replacePerson(Person oldPerson, Person newPerson){
+        int indexPerson = personList.indexOf(oldPerson);
+        if (indexPerson != PRODUCT_NOT_PRESENT){
+            personList.set(indexPerson, newPerson);
+        }
+    }
+
+    public void removePerson(String name, String info){
+        personList.indexOf(findIndex(name,info));
+    }
+
+    public int findIndex(String name, String info){
+        for ( int i = 0; i < personList.size(); i++){
+            Person person = personList.get(i);
+            if ( (person.getName() == name) && (person.getInfo() == info)){
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
