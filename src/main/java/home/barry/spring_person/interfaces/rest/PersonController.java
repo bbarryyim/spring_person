2982571c29e48e1559dfd5059845a0ed8908b8ea
barryyim <bbarryyim@gmail.com>
2019-10-29 18:10:30 -0700

    Changes made to the replace person function

--- src/main/java/home/barry/spring_person/interfaces/rest/PersonController.java
+++ src/main/java/home/barry/spring_person/interfaces/rest/PersonController.java
@@ -2,13 +2,11 @@ package home.barry.spring_person.interfaces.rest;
 
 import home.barry.spring_person.model.Person;
 import home.barry.spring_person.services.PersonService;
+import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/person")
@@ -20,14 +18,36 @@ public class PersonController {
     PersonService personService;
 
     @RequestMapping(value = "/add/", method = RequestMethod.POST)
-    public void createUser(@RequestBody Person person){
-        personService.addPerson(person);
-        logger.info("Added new Person : " + person.getName());
+    public void createUser(
+            @ApiParam(value = "Name of Person", required = true)
+            @RequestParam String name,
+            @ApiParam(value = "Person's info", required = true)
+            @RequestParam String info) {
+        personService.addPerson(new Person(name, info));
+        logger.info("Added new Person : " + name);
     }
 
     @RequestMapping(value = "/remove/", method = RequestMethod.DELETE)
-    public void removeUser(@RequestBody Person person){
-        personService.removePerson(person);
-        logger.info("Removed a Person : " + person.getName());
+    public void removeUser(
+            @ApiParam(value = "Name of Person", required = true)
+            @RequestParam String name,
+            @ApiParam(value = "Person's info", required = true)
+            @RequestParam String info) {
+        personService.removePerson(name, info);
+        logger.info("Removed a Person : " + name);
+    }
+
+    @RequestMapping(value = "/replace/", method = RequestMethod.POST)
+    public void replaceUser(
+            @ApiParam(value = "Name of Person to replace", required = true)
+            @RequestParam String nameOld,
+            @ApiParam(value = "Person's info to replace", required = true)
+            @RequestParam String infoOld,
+            @ApiParam(value = "Name of Person", required = true)
+            @RequestParam String name,
+            @ApiParam(value = "Person's info", required = true)
+            @RequestParam String info) {
+        personService.replacePerson(nameOld, infoOld, name, info);
+        logger.info("Replaced Person: " + nameOld + " with new Person: " + name);
     }
 }
