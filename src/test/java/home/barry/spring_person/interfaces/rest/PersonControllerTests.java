82624d412e25286719244cf7ef837fab3e4774be
barryyim <bbarryyim@gmail.com>
2019-11-02 10:42:36 -0700

    Correcting mockmvc

--- src/test/java/home/barry/spring_person/interfaces/rest/PersonControllerTests.java
+++ src/test/java/home/barry/spring_person/interfaces/rest/PersonControllerTests.java
@@ -1,45 +1,35 @@
 package home.barry.spring_person.interfaces.rest;
 
-import org.junit.Before;
+import home.barry.spring_person.services.PersonService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import static org.mockito.Mockito.mock;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@WebMvcTest(PersonController.class)
 public class PersonControllerTests {
 
     @Autowired
-    private PersonController personControllerTest;
-
     private MockMvc mockMvc;
 
-    @Before
-    public void setup() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(personControllerTest).build();
-    }
+    @MockBean
+    PersonService personService;
 
     @Test
     public void testCreateUserController() throws Exception{
-        personControllerTest = mock(PersonController.class);
 
-        this.mockMvc.perform(post("/add/")
-                .param("name", "John Doe")
-                .param("info", "John Doe"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value("John Doe"));
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                .post("/add/")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8).content("hello world")).andReturn();
     }
 
     @Test
